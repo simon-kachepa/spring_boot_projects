2acbccd1734a72f5cef580fd42674d46b458b843
simon Kachepa <kachepasimon@gmail.com>
2026-08-09 13:56:30 +0200

    Added the table of engineers to the database

--- spring_boot/src/main/java/com/kachepasimon/SoftwareEngineer.java
+++ spring_boot/src/main/java/com/kachepasimon/SoftwareEngineer.java
@@ -1,8 +1,15 @@
 package com.kachepasimon;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Objects;
 
+@Entity
 public class SoftwareEngineer {
+    @Id
     private Integer  id;
     private String name;
     private String techStack;
