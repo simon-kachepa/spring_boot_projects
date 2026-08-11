9aa7ba57d406126bb4cbc23e744ee84d32c0a1ac
simon Kachepa <kachepasimon@gmail.com>
2026-08-11 22:39:27 +0200

    Converted MyFirstClass class into a Bean using the annotation @Bean to make the IoC manage it so that I wont need to manually create instance of this class

--- spring_learning_one/src/main/java/com/kachepasimon/SpringLearningOneApplication.java
+++ spring_learning_one/src/main/java/com/kachepasimon/SpringLearningOneApplication.java
@@ -2,15 +2,21 @@ package com.kachepasimon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringLearningOneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLearningOneApplication.class, args);
+		var applicationContext = SpringApplication.run(SpringLearningOneApplication.class, args);
 
-		MyFirstClass myFirstClass = new MyFirstClass();
+		MyFirstClass myFirstClass = applicationContext.getBean(MyFirstClass.class);
 		System.out.println(myFirstClass.greeting());
+	}
+
+	@Bean
+	public MyFirstClass myFirstClass(){
+		return new MyFirstClass();
 	}
 
 }
