ad3af29955ae71c82ecf8e21a15eadc709de794f
simon Kachepa <kachepasimon@gmail.com>
2026-08-12 23:41:00 +0200

    Created POST method to post an object to the server using the annotation @POSTMAPPING and @REQUESTBODY

--- spring_learning_one/src/main/java/com/kachepasimon/FirstController.java
+++ spring_learning_one/src/main/java/com/kachepasimon/FirstController.java
@@ -1,13 +1,27 @@
 package com.kachepasimon;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
+    //Getting message from the server
+    @GetMapping("/get")
+    public String getMessage() {
         return "Hello World from my first controller";
+    }
+
+    //Sending message to the server
+    @PostMapping("/send")
+    public String sendMessage(@RequestBody String name) {
+        return "Hello World from: " + name;
+    }
+
+    @PostMapping("/send-order")
+    public String sendOrder(@RequestBody Order order) {
+        return "Hello World from Customer: " + order.toString();
     }
 }
---
+++ spring_learning_one/src/main/java/com/kachepasimon/Order.java
@@ -0,0 +1,41 @@
+package com.kachepasimon;
+
+public class Order {
+
+    private String customerName;
+    private String productName;
+    private Integer quantity;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "customerName='" + customerName + '\'' +
+                ", productName='" + productName + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
