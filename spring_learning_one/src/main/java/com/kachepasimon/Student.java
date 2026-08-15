649cbc35bd14cc6ff0122c901e993ade1945773e
simon Kachepa <kachepasimon@gmail.com>
2026-08-16 01:00:49 +0200

    Created the first entity and created a table in the database Necessary annotations @Entity to show that this class is an entity that will need to be mapped to a relational database @Id this denotes the primary key of our table. Every entity requires a primary key. @Table used to give/specify name to our table in the database. By default, the Entity name is used

---
+++ spring_learning_one/src/main/java/com/kachepasimon/Student.java
@@ -0,0 +1,58 @@
+package com.kachepasimon;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "students")
+public class Student {
+
+    @Id
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+    public Student() {
+    }
+    public Student(String firstName, String lastName, String email, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
