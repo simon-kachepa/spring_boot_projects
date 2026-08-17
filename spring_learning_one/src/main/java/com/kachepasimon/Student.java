64eb2e049de1683c4e8d118619eb87dc8f974564
simon Kachepa <kachepasimon@gmail.com>
2026-08-17 08:32:28 +0200

    Created endpoint that fetch all students from the database and return them as a list

--- spring_learning_one/src/main/java/com/kachepasimon/Student.java
+++ spring_learning_one/src/main/java/com/kachepasimon/Student.java
@@ -1,14 +1,13 @@
 package com.kachepasimon;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name= "students")
+@Table(name="students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
     private String firstName;
     private String lastName;
@@ -22,6 +21,14 @@ public class Student {
         this.lastName = lastName;
         this.email = email;
         this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getFirstName() {
---
+++ spring_learning_one/src/main/java/com/kachepasimon/StudentController.java
@@ -0,0 +1,32 @@
+package com.kachepasimon;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @PostMapping("/students")
+    public Student createStudent(
+            @RequestBody Student student
+    ) {
+        return studentRepository.save(student);
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudent() {
+        return studentRepository.findAll();
+    }
+
+
+}
