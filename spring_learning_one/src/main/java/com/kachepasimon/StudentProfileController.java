a6cd48ef9631cdf77f57c29502fc830e7b06df9b
simon Kachepa <kachepasimon@gmail.com>
2026-08-19 15:03:13 +0200

    Added the Controllers and Repository for the School and Profile

---
+++ spring_learning_one/src/main/java/com/kachepasimon/StudentProfileController.java
@@ -0,0 +1,4 @@
+package com.kachepasimon;
+
+public class StudentProfileController {
+}
---
+++ spring_learning_one/src/main/java/com/kachepasimon/StudentProfileRepository.java
@@ -0,0 +1,6 @@
+package com.kachepasimon;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentProfileRepository extends JpaRepository<StudentProfile, Integer> {
+}
