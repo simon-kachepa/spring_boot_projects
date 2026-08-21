0575cef0082cd87c880bfe41f1516193563ce5dc
simon Kachepa <kachepasimon@gmail.com>
2026-08-21 23:16:15 +0200

    Added the post method in the school using @PostMapping

--- spring_learning_one/src/main/java/com/kachepasimon/SchoolController.java
+++ spring_learning_one/src/main/java/com/kachepasimon/SchoolController.java
@@ -1,8 +1,6 @@
 package com.kachepasimon;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class SchoolController {
     @GetMapping
     public List<School> getAllSchools() {
         return schoolRepository.findAll();
+    }
+
+    @PostMapping
+    public School createSchool(@RequestBody School school) {
+        return schoolRepository.save(school);
     }
 }
