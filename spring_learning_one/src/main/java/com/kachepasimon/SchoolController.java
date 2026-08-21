e70f6356173b19b09a4deab4eec1c2e3574aa9a5
simon Kachepa <kachepasimon@gmail.com>
2026-08-21 23:14:15 +0200

    Added the get method in the School controller using the annotation @GetMapping and imported the SchoolRepository

--- spring_learning_one/src/main/java/com/kachepasimon/SchoolController.java
+++ spring_learning_one/src/main/java/com/kachepasimon/SchoolController.java
@@ -1,4 +1,23 @@
 package com.kachepasimon;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/schools")
 public class SchoolController {
+
+    private final SchoolRepository schoolRepository;
+
+    public SchoolController(SchoolRepository schoolRepository) {
+        this.schoolRepository = schoolRepository;
+    }
+
+    @GetMapping
+    public List<School> getAllSchools() {
+        return schoolRepository.findAll();
+    }
 }
