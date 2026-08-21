d6aeb6bec1d8d72281cd30e172c1e2563aa9a0dd
simon Kachepa <kachepasimon@gmail.com>
2026-08-21 23:21:19 +0200

    Added the Get Method in the student Profiles controller

--- spring_learning_one/src/main/java/com/kachepasimon/StudentProfileController.java
+++ spring_learning_one/src/main/java/com/kachepasimon/StudentProfileController.java
@@ -1,4 +1,23 @@
 package com.kachepasimon;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/student-profiles")
 public class StudentProfileController {
+
+    private final StudentProfileRepository studentProfileRepository;
+
+    public StudentProfileController(StudentProfileRepository studentProfileRepository) {
+        this.studentProfileRepository = studentProfileRepository;
+    }
+
+    @GetMapping
+    public List<StudentProfile> getAllStudentProfiles() {
+        return studentProfileRepository.findAll();
+    }
 }
