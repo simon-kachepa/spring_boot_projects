7f4985c23fa958cda4ef04c9eef5c2ce4dcee241
simon Kachepa <kachepasimon@gmail.com>
2026-09-17 07:42:57 +0200

    Added the setUp() in the test, by annotating the method with @BeforeEach. This method is run before each other test method. It is used to initialize common fields that are required by the methods

--- spring_learning_one/src/test/java/com/kachepasimon/school/SchoolServiceTest.java
+++ spring_learning_one/src/test/java/com/kachepasimon/school/SchoolServiceTest.java
@@ -1,14 +1,20 @@
 package com.kachepasimon.school;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyFirstTest {
 
+    @BeforeEach
+    void setUp() {
+        System.out.println("Hello, inside BeforeEach");
+    }
+
     @Test
     public void myFirstTest() {
-        System.out.println("myFirstTest");
+        System.out.println("Hello, inside myFirstTest");
     }
 
 }
