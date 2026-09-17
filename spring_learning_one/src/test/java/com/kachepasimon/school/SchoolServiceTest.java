e415752a1185f0d8094bdf01b2350e91cb4836d5
simon Kachepa <kachepasimon@gmail.com>
2026-09-17 07:38:13 +0200

    Added the first Test method

---
+++ spring_learning_one/src/test/java/com/kachepasimon/school/SchoolServiceTest.java
@@ -0,0 +1,14 @@
+package com.kachepasimon.school;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyFirstTest {
+
+    @Test
+    public void myFirstTest() {
+        System.out.println("myFirstTest");
+    }
+
+}
